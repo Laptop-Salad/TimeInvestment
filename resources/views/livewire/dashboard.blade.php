6da07d85f865fc00c6426cb36b2d00631f68a87b
Laptop-Salad <awallis1609@gmail.com>
2024-12-28 21:50:54 +0000

    ui: rois on dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -15,7 +15,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <x-card class="p-5 grid grid-cols-2">
+            <x-card class="p-5 grid grid-cols-3">
                 <p class="font-semibold text-lg">
                     {{ __('Investment so far') }}:
                     <span class="text-yellow-500 ms-4">
@@ -28,6 +28,13 @@
                     <span class="text-blue-500 ms-4">
                         <i class="fa-duotone fa-regular fa-clock me-2"></i>
                         {{auth()->user()->coins->sum('hours_spent')}}
+                    </span>
+                </p>
+                <p class="font-semibold text-lg border-s ps-2">
+                    {{ __('ROIs') }}:
+                    <span class="text-pink-500 ms-4">
+                        <i class="fa-solid fa-chart-mixed-up-circle-dollar me-2"></i>
+                        {{auth()->user()->coins->sum(fn($c) => $c->rois->count() ?? 0)}}
                     </span>
                 </p>
             </x-card>
