cd630c2b9f2097be076923113a4e18ea9e0a2485
Laptop-Salad <awallis1609@gmail.com>
2025-02-21 09:26:11 +0000

    cosmetic(dashboard): remove unused imports

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,16 +2,7 @@
 
 namespace App\Livewire;
 
-use App\Enums\InvestmentType;
-use App\Enums\Status;
-use App\Livewire\Forms\InvestmentForm;
-use App\Livewire\Forms\Dashboard\FilterForm;
-use App\Models\Investment;
-use Carbon\Carbon;
-use Livewire\Attributes\Computed;
-use Livewire\Attributes\On;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class Dashboard extends Component
 {
